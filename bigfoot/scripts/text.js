0983f3492a404fe18c53fe25cd08e2dd566e8108
tw31120071857 <93863760+tw31120071857@users.noreply.github.com>
2022-12-31 12:42:03 +1100

    1

---
+++ bigfoot/scripts/text.js
@@ -0,0 +1,32 @@
+const i = Math.floor(r_text.length * Math.random());
+const r_text = [
+    "Infinite Possibilities",
+    "Bigfoot's Game Shack",
+    "Is Bigfoot real?",
+    "Don't Cry because its over, smile because it happened...",
+    "Relationships are like farts, if you have to force them they are proably shit...",
+    "Are we living in a simulation?",
+    "The cyberwar continues...",
+    "Who would win: Bean Tower or Fruit Tower?",
+    "What are the chances?",
+    "Bigfoot must be the best at hide and seek",
+    "Thanks for using Bigfoots Game Shack!",
+    "More unblocked games at 3kh0.github.io!",
+    "More Unblocked games at Arr3.gq!",
+    "Warm and Colddddddd.",
+    "Im so tired and awake.",
+    "Imagine not sharing this website with your friends",
+    "Never gonna give you up, never gonna let you down...",
+    "If you haven't noticed, my favorite color is lime.",
+    "Can you do my math homework?",
+    "The manager gets paid less...",
+    "Is that a phone by your knee? -Kahlil Parker 2022",
+    "Time is irrelevant like everything else except this website...",
+    "Did you know the three dots are called elipses...",
+    "Grapes light up in flames in the microwave...",
+    "Did you know that Mcdonalds made buble gum flavored broccoli?",
+    "Will you marry me?"
+];
+
+document.write(`<span class="textjs">${i}</span>`);
+

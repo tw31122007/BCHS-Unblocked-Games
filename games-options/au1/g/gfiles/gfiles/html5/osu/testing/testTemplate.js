1693bd2e173b739167f92ad913b6ca55dec6fc32
tw31120071857 <93863760+tw31120071857@users.noreply.github.com>
2023-05-03 13:37:14 +1000

    1

--- games-options/au1/g/gfiles/gfiles/html5/osu/testing/testTemplate.js
+++
@@ -1,5 +0,0 @@
-define(function(require) {
-	let Test = require("../test.js");
-	Formulas = require("../../src/scripts/Formulas.js");
-	Test.table(function, [arguments], [expected values]);
-});
--- games-options/bigfoot-only-proxied-games/scripts/opensettingstab.js
+++
@@ -1,8 +0,0 @@
-function opensettings(){
-    const id = document.getElementById("opensettingsid");
-    if (id.style.display === "none") {
-        id.style.display = "block";
-      } else {
-        id.style.display = "none";
-      }
-}      opensettings();

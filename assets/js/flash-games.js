094ed6af1ab35ebff2f9b44f9872790d1f01759f
tw31120071857 <93863760+tw31120071857@users.noreply.github.com>
2023-10-06 14:26:08 +1100

    1

---
+++ assets/js/flash-games.js
@@ -0,0 +1,23 @@
+function setIframeType(type) {
+    localStorage.setItem('iframe-type', type);
+}
+
+// Get modal and buttons
+const modal = document.getElementById('flashPlayerModal');
+const okButton = document.getElementById('okButton');
+const downloadButton = document.getElementById('downloadButton');
+
+// Show modal on page load
+window.onload = function() {
+    modal.style.display = 'block';
+}
+
+// Hide modal when "Ok" is clicked
+okButton.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// Open Flash Player download page in a new tab when "Download Flash Player" is clicked
+downloadButton.onclick = function() {
+    window.open("/flash-player.html", "_blank"); 
+}

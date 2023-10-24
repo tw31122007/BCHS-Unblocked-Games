4bbdb45c9a442b99ca8d0300d2a5a88539e21b2f
tw31122007 <tw31122007@googlemail.com>
2023-10-24 18:30:53 +1100

    1

--- assets/js/info.js
+++ assets/js/info.js
@@ -1,35 +1,37 @@
 const tabs = document.querySelectorAll('.tab');
-
 const contentDiv = document.querySelector('.tab-content');
 const urlBar = document.querySelector('.url-bar');
-// Function to set the default URL when the page loads
-function setDefaultURL() {
-    urlBar.value = `https://${window.location.hostname}/tab1`;
+const videoElement = contentDiv.querySelector('video');
+const videoSourceElement = videoElement.querySelector('source');
+
+// Function to set the default URL and video when the page loads
+function setDefaults() {
+    urlBar.value = `https://${window.location.hostname}/`;
+    videoSourceElement.setAttribute('src', '/assets/img/tutorial/info-tab-1.mp4');
+    videoElement.load();
+    videoElement.play();
 }
 
-// Function to update the URL bar based on the clicked tab
-function updateURLBar(tabId) {
-    urlBar.value = `https://${window.location.hostname}/${tabId}`;
+// Function to update the URL bar and video based on the clicked tab
+function updateTabContent(suffix, videoSrc) {
+    urlBar.value = `https://${window.location.hostname}/${suffix}`;
+    videoSourceElement.setAttribute('src', videoSrc);
+    videoElement.load();
+    videoElement.play();
 }
 
-// Call setDefaultURL to set the default URL when the page loads
-setDefaultURL();
+// Call setDefaults to set the default URL and video when the page loads
+setDefaults();
 
 tabs.forEach(tab => {
     tab.addEventListener('click', function() {
+        // Update active class for tabs
+        tabs.forEach(innerTab => innerTab.classList.remove('active'));
+        this.classList.add('active');
 
-        // Update URL bar with the domain and clicked tab info
-        const tabId = this.id;
-        updateURLBar(tabId);
-    });
-});
-
-document.querySelectorAll('.tab').forEach(tab => {
-    tab.addEventListener('click', function() {
-        let contentId = 'content-' + this.id;
-        document.querySelectorAll('.tab-content').forEach(content => {
-            content.style.display = 'none';  // Hide all tab contents
-        });
-        document.getElementById(contentId).style.display = 'block';  // Show the clicked tab's content
+        // Update URL bar and video based on the clicked tab
+        const urlSuffix = this.getAttribute('data-url-suffix');
+        const videoSrc = this.getAttribute('data-video-src');
+        updateTabContent(urlSuffix, videoSrc);
     });
 });

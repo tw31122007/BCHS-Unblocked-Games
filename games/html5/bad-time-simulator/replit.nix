a098c643d0ef4c2a77d70a38ba115b402734bb89
tw31120071857 <tw31122007@googlemail.com>
2023-10-28 21:46:26 +1100

    1

--- games/html5/bad-time-simulator/replit.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.zip
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
-  ];
-}
--- games/html5/sm64/_OLD_/README.md
+++
@@ -1,2 +0,0 @@
-# files
-files in this folder should work properly but don't work so an older version is used on the gh page

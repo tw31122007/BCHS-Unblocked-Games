3fd28fc2d7b11a9c1182405ff6fd2570de0b81e8
tw31122007 <tw31122007@googlemail.com>
2024-02-01 13:45:13 +1100

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://bare.desmoscalc.me',
+    bare: 'https://bare.notapr0xy.com.au',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

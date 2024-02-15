f0fc0e1fd293fbaf3d6a911f7468937479d0cecc
tw31122007 <tw31122007@googlemail.com>
2024-02-16 10:07:53 +1100

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://shuttle.notapr0xy.com.au/bare/',
+    bare: 'https://shuttle.alegithomeworksite.online/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

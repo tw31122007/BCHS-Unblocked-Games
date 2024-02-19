67b4573df2123a5725eb62abe42f580a7e0bb3f2
tw31120071857 <tw31122007@googlemail.com>
2024-02-19 14:15:13 +1100

    1

--- assets/js/openurl.js
+++ assets/js/openurl.js
@@ -61,7 +61,7 @@ function setWaflashURL(url) {
 
 function setGameInfo(gameName, appName, prefix, iframeType, showNameAndImg) {
     function constructURL(gameName, prefix) {
-        var allowedDomains = ['desmoscalc.me', 'hehemaths.gq', 'defnotgxmes.ink', 'alegithomeworksite.com.au', 'alegithomeworksite.eu.org', 'alegithomeworksite.online', 'bchs-unblocked.tk', 'defmaths.online', 'geologyrocks.com.au', 'imrunningoutofideas.com.au', 'imrunningoutofideas.online', 'elephantmunchingcontest.com.au', 'elephantmunchingcontest.site', 'elephantmunchingcontest.tech', 'lolmath.tk', 'notapr0xy.com.au', 'notapr0xy.online', 'probnotschoolwork.ink', 'the-toaster-is-a.ninja', 'thisisatotallylegithomeworkwebsite.com.au', 'thisisatotallylegithomeworkwebsite.online']; // List of allowed domains
+        var allowedDomains = ['desmoscalc.me', 'hehemaths.gq', 'defnotgxmes.ink', 'alegithomeworksite.com.au', 'alegithomeworksite.eu.org', 'alegithomeworksite.online', 'bchs-unblocked.tk', 'defmaths.online', 'geologyrocks.com.au', 'imrunningoutofideas.com.au', 'imrunningoutofideas.online', 'elephantmunchingcontest.com.au', 'elephantmunchingcontest.site', 'elephantmunchingcontest.tech', 'lolmath.tk', 'notapr0xy.com.au', 'notapr0xy.online', 'probnotschoolwork.ink', 'the-toaster-is-a.ninja', 'thisisatotallylegithomeworkwebsite.com.au', 'thisisatotallylegithomeworkwebsite.online', 'pizzafuneral.com.au', 'pizzafuneral.online', 'pizzafuneral.site', 'pizzafuneral.store']; // List of allowed domains
         var defaultDomain = 'notapr0xy.com.au'; // Default domain to use if not in the allowed list
         var currentDomain = window.location.hostname;
 

4e6b1c80e030acd954b1923e33a4ef3e1b880a15
tw31122007 <tw31122007@googlemail.com>
2024-02-23 09:40:09 +1100

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://shuttle.bchsubl0ckedgxmes.co.uk/bare/',
+    bare: 'https://bare.bchsubl0ckedgxmes.co.uk',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

2f8ffbd41aa8a378c162d7f7eac657c13237b931
tw31122007 <tw31122007@googlemail.com>
2024-02-23 09:59:19 +1100

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://shuttle.thefastestlearner.store/bare/',
+    bare: 'https://bare.thefastestlearner.store',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

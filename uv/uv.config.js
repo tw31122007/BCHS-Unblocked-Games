43d50424144083733d3d4e3cb8a05fc0fa7ab299
tw31122007 <tw31122007@googlemail.com>
2024-02-23 10:01:42 +1100

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://shuttle.the-toaster-is-a.ninja/bare/',
+    bare: 'https://bare.the-toaster-is-a.ninja',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

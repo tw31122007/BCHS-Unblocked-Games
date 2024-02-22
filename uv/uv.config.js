1aff42066f29e70d81a997d579c9a89631d8e171
tw31122007 <tw31122007@googlemail.com>
2024-02-23 09:48:41 +1100

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://shuttle.pizzafuneral.com.au/bare/',
+    bare: 'https://bare.pizzafuneral.com.au',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

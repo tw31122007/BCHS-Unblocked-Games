5ff6f9a0656914083232e435f761eeb3fe36c667
tw31122007 <tw31122007@googlemail.com>
2024-02-23 10:05:51 +1100

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://shuttle.tylerisawesome.uk/bare/',
+    bare: 'https://bare.tylerisawesome.uk',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

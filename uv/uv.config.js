5409a8c649709d13979e7a540cf6191c8eb09ecd
tw31120071857 <tw31122007@googlemail.com>
2024-09-27 18:54:53 +1000

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://bare.thisisatotallylegithomeworkwebsite.com.au',
+    bare: 'https://bare.thisisatotallylegithomeworkwebsite.com.au/bear/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv/uv.handler.js',

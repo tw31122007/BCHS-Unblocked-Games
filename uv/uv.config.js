733c61cac5d6c2c5839288648d0f95002e6d1a5b
tw31120071857 <tw31122007@googlemail.com>
2024-09-27 18:52:06 +1000

    1

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://bare.probnotschoolwork.ink',
+    bare: 'https://bare.probnotschoolwork.ink/bear/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv/uv.handler.js',
